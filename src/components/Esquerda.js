ff96692c2dcce17911f291c3bf121a7200543b9e
Glauco Pantoni <glaucopantoni@gmail.com>
2021-12-05 19:30:32 -0300

    Divisão <Esquerda /> e <Sidebar /> implementadas

---
+++ src/components/Esquerda.js
@@ -0,0 +1,11 @@
+import Posts from "./Posts";
+import Stories from "./Stories";
+
+export default function Esquerda() {
+    return (
+        <div class="esquerda">
+            <Stories />
+            <Posts />
+        </div>
+    )
+}
